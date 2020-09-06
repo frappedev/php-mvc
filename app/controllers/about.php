529cebd0bf3ee89992e1f0f358962cab7710f1aa
Harpreet Singh <thesinghharpreet@gmail.com>
2020-09-06 23:00:54 +0530

    make routing system

---
+++ app/controllers/about.php
@@ -0,0 +1,14 @@
+<?php
+
+class about {
+
+	public function index()
+	{
+		echo 'hello, i am anout page';
+	}
+
+	public function test()
+	{
+		echo 'i am test';
+	}
+}
--- app/controllers/home.php
+++ app/controllers/home.php
@@ -1,5 +1,14 @@
 <?php
 
-class home extends Controller {
+class home {
 
+	public function index($name = '')
+	{
+		echo 'hello, i am ' . $name;
+	}
+
+	public function test()
+	{
+		echo 'i am test';
+	}
 }
